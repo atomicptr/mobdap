8565a5a1b8696c1a8713616099805f9537701eff
Christopher Kaster <me@atomicptr.de>
2025-08-03 11:38:25 +0200

    add simple cli flags for --help and --version

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject mobdap "0.1.0"
+(defproject mobdap "0.1.0-alpha2"
   :description "Debug Adapter for MobDap"
   :url "https://github.com/atomicptr/mobdap"
   :license {:name "GPL-3.0-or-later" :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[cheshire/cheshire                 "6.0.0"]
+                 [org.clojure/tools.cli             "1.1.230"]
                  [clj-stacktrace                    "0.2.8"]
                  [com.taoensso/timbre               "6.7.1"]
                  [org.clojure/clojure               "1.12.1"]
--- src/mobdap/main.clj
+++ src/mobdap/main.clj
@@ -1,6 +1,7 @@
 (ns mobdap.main
   (:require
    [clojure.java.io :as io]
+   [clojure.tools.cli :refer [parse-opts]]
    [mobdap.handler :as handler]
    [taoensso.timbre :as log]
    [taoensso.timbre.appenders.core :as appenders])
@@ -11,11 +12,39 @@
    (System/getenv "XDG_CACHE_HOME")
    (str (io/file (System/getProperty "user.home") ".cache"))))
 
-(defn -main [& args]
-  (log/set-config!
-   {:level :info
-    :appenders {:spit (appenders/spit-appender {:fname (str (io/file (cache-dir) "mobdap.log"))})}})
-  (log/info "Started mobdap with arguments:" args)
-  (handler/run))
+(def cli-options
+  [["-v" "--version" "Show the installed version of mobdap"]
+   ["-h" "--help"    "Show the help text"]])
 
+(defn- print-usage [result]
+  (println "Usage:")
+  (println (str "\t" "mobdap [options]"))
+  (println)
+  (println (:summary result)))
+
+(defn -main [& args]
+  (let [res (parse-opts args cli-options)]
+    (cond
+      ; if we had errors
+      (not-empty (:errors res)) (do (println "Error:")
+                                    (doseq [err (:errors res)]
+                                      (println (str "\t" err)))
+                                    (println)
+                                    (print-usage res)
+                                    (System/exit 1))
+
+      ; --help
+      (get-in res [:options :help]) (print-usage res)
+
+      ; --version
+      (get-in res [:options :version]) (println (System/getProperty "mobdap.version"))
+
+      ; run the app
+      :else
+      (do
+        (log/set-config!
+         {:level :info
+          :appenders {:spit (appenders/spit-appender {:fname (str (io/file (cache-dir) "mobdap.log"))})}})
+        (log/info "Started mobdap with arguments:" args)
+        (handler/run)))))
 
