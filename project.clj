790273bb990879406e4be3f4507d66a885b78a95
Christopher Kaster <me@atomicptr.de>
2025-09-01 12:53:28 +0200

    update dependencies, add lein ancient plugin

--- project.clj
+++ project.clj
@@ -5,12 +5,13 @@
   :dependencies [[cheshire/cheshire                 "6.0.0"]
                  [org.clojure/tools.cli             "1.1.230"]
                  [clj-stacktrace                    "0.2.8"]
-                 [com.taoensso/timbre               "6.7.1"]
-                 [org.clojure/clojure               "1.12.1"]
+                 [com.taoensso/timbre               "6.8.0"]
+                 [org.clojure/clojure               "1.12.2"]
                  [org.clojure/core.async            "1.8.741"]
                  [org.luaj/luaj-jse                 "3.0.1"]]
   :plugins      [[io.taylorwood/lein-native-image   "0.3.1"]
-                 [lein-set-version/lein-set-version "0.4.1"]]
+                 [lein-set-version/lein-set-version "0.4.1"]
+                 [lein-ancient                      "1.0.0-RC3"]]
   :main ^:skip-aot mobdap.main
   :omit-source true
   :source-paths ["src"]
@@ -28,7 +29,7 @@
               :dependencies [[nrepl/nrepl "1.3.1"]]}
 
              :test
-             {:dependencies [[nubank/matcher-combinators "3.9.1"]]}
+             {:dependencies [[nubank/matcher-combinators "3.9.2"]]}
 
              :uberjar
              {:main mobdap.main
